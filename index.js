c29520fca649d3f1798aad7e079eeedbd2773333
mengshalitsofi <mengshalitsofi@gmail.com>
2021-04-20 13:31:41 -0700

    commit tree 4d8e760d0a1e09dbb2547ff05862338fb1555d5f

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"];
+
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log('Thank you, ${cards[i]}, for the wonderful surprise gift!');
+  }
+
+  return cards;
+}
+
+writeCards(cards);
+
+let countdown = 0;
+function countDown() {
+    for (let countdown = 0; countdown < 10; countdown++) {
+        console.log(countdown);
+      } 
+}
